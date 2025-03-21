bd043fd045cf54a779cc046a459382da9a0455a4
OA Wu <oawu.tw@gmail.com>
2025-03-21 10:19:11 +0800

    fix code.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @author      OA Wu <oawu.tw@gmail.com>
- * @copyright   Copyright (c) 2015 - 2022, @oawu/xterm
+ * @copyright   Copyright (c) 2015 - 2025, @oawu/xterm
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
